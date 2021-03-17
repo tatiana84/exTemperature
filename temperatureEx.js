e49a469f50082a8d8e519a4b1b4a0f468e998a6a
tatiana84 <tantamuntean2018@gmail.com>
2021-03-17 19:28:48 +0200

    First commit

---
+++ temperatureEx.js
@@ -0,0 +1,16 @@
+const kelvin = 293;//it's a constant variable
+//console.log(kelvin - 273);//print: 20
+
+let celsius = 20;
+celsius = 19;
+//console.log(celsius*(9/5)+32);//print: 68
+//let fahrenheit = 68;
+
+//console.log(celsius*(9/5)+32);//print: 66.2
+let fahrenheit = 66.2;
+//console.log(Math.floor(66.2)); // print: 66
+
+fahrenheit = 66;
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+
